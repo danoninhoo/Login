4be39fb3a24a92a82df992695e4e1545220e30f7
danoninhoo <73741200+danoninhoo@users.noreply.github.com>
2021-05-05 12:10:39 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,12 +3,7 @@ import firebase from "firebase/app";
 import "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD8JAsVVAuXGGA3CAkgEFY7BnggYsqjMBg",
-    authDomain: "mealfind-testdb.firebaseapp.com",
-    projectId: "mealfind-testdb",
-    storageBucket: "mealfind-testdb.appspot.com",
-    messagingSenderId: "710487185799",
-    appId: "1:710487185799:web:32942a1e9435370be31b22"
+
   };
   
   // Initialize Firebase
